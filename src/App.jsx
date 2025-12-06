a30b1f982c761ecccea3b1ada5a95438bb48095c
Julia Khabibova <julia.khabibova@mail.ru>
2025-12-06 19:41:28 +0300

    gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import { useMachine } from "@xstate/react";
 import { videoPlayerMachine } from "./machines/videoPlayerMachine";
+import { MaxIcon, MinIcon, PlayIcon, StopIcon, CloseIcon, OpenIcon } from "./utils/Icons";
 import "./App.css";
 
 function App() {
@@ -40,10 +41,7 @@ function App() {
   };
 
   return (
-    <div className={`video-app ${
-      (isNormal || isMinimized)  ? "video-app-grey" : ""
-    }`}>
-
+    <div className="video-app">
       <div
         className={`video-app__controls ${
           isClosed ? "" : "video-app__controls--hidden"
@@ -53,73 +51,73 @@ function App() {
           <button
             className="open-button"
             onClick={() => send({ type: "OPEN" })}
-          >
-          <svg viewBox="64 64 896 896" focusable="false" data-icon="play-circle" width="50px" height="50px" fill="#8585ff" aria-hidden="true"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path><path d="M719.4 499.1l-296.1-215A15.9 15.9 0 00398 297v430c0 13.1 14.8 20.5 25.3 12.9l296.1-215a15.9 15.9 0 000-25.8zm-257.6 134V390.9L628.5 512 461.8 633.1z"></path></svg>
+          > <OpenIcon />
+            
           </button>
         )}
       </div>
 
       {(isNormal || isMinimized) && (
-        <div className="video-player-container">
-        <div
-          className={`video-player ${
-            isMinimized ? "video-player--minimized" : ""
-          }`}
-        >
-          <div className="video-app__title-container">
-          <p className="video-app__title">Player</p>
-          <button
+        <div className="video-player__container" onClick={handleClose}>
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className={`video-player ${
+              isMinimized ? "video-player--minimized" : ""
+            }`}
+          >
+            <div className="video-app__title-container">
+              <p className="video-app__title">Player</p>
+              <button
                 className="control-button control-button--close"
                 onClick={handleClose}
               >
-                <svg viewBox="64 64 896 896" focusable="false" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path></svg>
+                <CloseIcon />
               </button>
-          </div>
-          <div className="video-player__container">
-          <video
-            ref={videoRef}
-            src={url}
-            className="video-player__video"
-            onError={(e) => console.log("Ошибка видео:", e)}
-            controls={false}
-          />
-          </div>
-          <div className="video-player__controls">
-            <button
-              className={getPlayButtonClass()}
-              onClick={() => {
-                if (playing) {
-                  send({ type: "PAUSE" });
-                } else {
-                  send({ type: "PLAY" });
-                }
-              }}
-            >
-              {playing ? <svg viewBox="64 64 896 896" focusable="false" data-icon="pause" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M304 176h80v672h-80zm408 0h-64c-4.4 0-8 3.6-8 8v656c0 4.4 3.6 8 8 8h64c4.4 0 8-3.6 8-8V184c0-4.4-3.6-8-8-8z"></path></svg> : <svg viewBox="0 0 1024 1024" focusable="false" data-icon="caret-right" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M715.8 493.5L335 165.1c-14.2-12.2-35-1.2-35 18.5v656.8c0 19.7 20.8 30.7 35 18.5l380.8-328.4c10.9-9.4 10.9-27.6 0-37z"></path></svg>}
-            </button>
+            </div>
+            <div className="video-player-container">
+              <video
+                ref={videoRef}
+                src={url}
+                className="video-player__video"
+                onError={(e) => console.log("Ошибка видео:", e)}
+                controls={false}
+              />
+            </div>
+            <div className="video-player__controls">
+              <button
+                className={getPlayButtonClass()}
+                onClick={() => {
+                  if (playing) {
+                    send({ type: "PAUSE" });
+                  } else {
+                    send({ type: "PLAY" });
+                  }
+                }}
+              >
+                {playing ? <StopIcon /> : <PlayIcon />}
+              </button>
 
-            <div className="button-group">
-              {isNormal && (
-                <button
-                  className="control-button control-button--minimize"
-                  onClick={() => send({ type: "MINIMIZE" })}
-                >
-                  <svg viewBox="64 64 896 896" focusable="false" data-icon="shrink" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M881.7 187.4l-45.1-45.1a8.03 8.03 0 00-11.3 0L667.8 299.9l-54.7-54.7a7.94 7.94 0 00-13.5 4.7L576.1 439c-.6 5.2 3.7 9.5 8.9 8.9l189.2-23.5c6.6-.8 9.3-8.8 4.7-13.5l-54.7-54.7 157.6-157.6c3-3 3-8.1-.1-11.2zM439 576.1l-189.2 23.5c-6.6.8-9.3 8.9-4.7 13.5l54.7 54.7-157.5 157.5a8.03 8.03 0 000 11.3l45.1 45.1c3.1 3.1 8.2 3.1 11.3 0l157.6-157.6 54.7 54.7a7.94 7.94 0 0013.5-4.7L447.9 585a7.9 7.9 0 00-8.9-8.9z"></path></svg>
-                </button>
-              )}
+              <div className="button-group">
+                {isNormal && (
+                  <button
+                    className="control-button control-button--minimize"
+                    onClick={() => send({ type: "MINIMIZE" })}
+                  >
+                    <MinIcon />
+                  </button>
+                )}
 
-              {isMinimized && (
-                <button
-                  className="control-button control-button--maximize"
-                  onClick={() => send({ type: "MAXIMIZE" })}
-                >
-                  <svg viewBox="64 64 896 896" focusable="false" data-icon="arrows-alt" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M855 160.1l-189.2 23.5c-6.6.8-9.3 8.8-4.7 13.5l54.7 54.7-153.5 153.5a8.03 8.03 0 000 11.3l45.1 45.1c3.1 3.1 8.2 3.1 11.3 0l153.6-153.6 54.7 54.7a7.94 7.94 0 0013.5-4.7L863.9 169a7.9 7.9 0 00-8.9-8.9zM416.6 562.3a8.03 8.03 0 00-11.3 0L251.8 715.9l-54.7-54.7a7.94 7.94 0 00-13.5 4.7L160.1 855c-.6 5.2 3.7 9.5 8.9 8.9l189.2-23.5c6.6-.8 9.3-8.8 4.7-13.5l-54.7-54.7 153.6-153.6c3.1-3.1 3.1-8.2 0-11.3l-45.2-45z"></path></svg>
-                </button>
-              )}
-
+                {isMinimized && (
+                  <button
+                    className="control-button control-button--maximize"
+                    onClick={() => send({ type: "MAXIMIZE" })}
+                  >
+                    <MaxIcon />
+                  </button>
+                )}
+              </div>
             </div>
           </div>
-        </div>
         </div>
       )}
     </div>
